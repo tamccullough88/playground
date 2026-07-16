349f05ac733bd848308c6cc97da2e500872af8bb
Thomas McCullough <tamccullough88@gmail.com>
2026-07-16 11:46:04 -0400

    Add Google Analytics tracking script

---
+++ assets/tracking.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V3TRVWQCL6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V3TRVWQCL6');
+</script>
